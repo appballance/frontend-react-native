d206651bdc019ae32fb058f4562304769885722c
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-21 18:31:46 -0300

    Edit from have configured in use

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,8 @@
 export const SETTINGS = {
+  BASE_URL: 'https://czdigital.herokuapp.com',
   ROUTES_BACKEND: {
-    CREATE_USER: '/users',
-    GET_USER: '/users',
-    AUTH_USER: '/auth',
+    CREATE_USER: 'users',
+    GET_USER: 'users',
+    AUTH_USER: 'auth',
   },
 };
