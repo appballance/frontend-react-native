05e4d516685128137a22fe90042985f4c3939b14
João pedro <joaof082@gmail.com>
2022-11-28 12:24:43 -0300

    feat: adding theme provider
    Signed-off-by: João pedro <joaof082@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import { Routes } from 'application /routes';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from 'views/styles/theme';
 
 function App() {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
