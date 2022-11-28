af8b3ba2656656747070860e5ea658564cd3ec88
João pedro <joaof082@gmail.com>
2022-11-28 12:26:19 -0300

    refact: replacing elements html to elements react native
    Signed-off-by: João pedro <joaof082@gmail.com>

--- src/views/Components/Typography/styles.js
+++ src/views/Components/Typography/styles.js
@@ -1,20 +1,20 @@
 import styled from 'styled-components';
 
-const h1 = styled.h1`
+const h1 = styled.Text`
   font-size: ${({ theme }) => theme.typography.h1?.fontSize};
   color: ${({ theme }) => theme.palette.colors.text};
   font-weight: ${({ theme }) => theme.typography.h1?.fontWeight};
   font-family: ${({ theme }) => theme.typography.h1?.fontFamily};
 `;
 
-const h2 = styled.h2`
+const h2 = styled.Text`
   font-size: ${({ theme }) => theme.typography.h2?.fontSize};
   color: ${({ theme }) => theme.palette.colors.text};
   font-weight: ${({ theme }) => theme.typography.h2?.fontWeight};
   font-family: ${({ theme }) => theme.typography.h2?.fontFamily};
 `;
 
-const body1 = styled.p`
+const body1 = styled.Text`
   font-size: ${({ theme }) => theme.typography.body1?.fontSizeText};
   color: ${({ theme }) => theme.palette.colors.text};
   font-weight: ${({ theme }) => theme.typography.body1?.fontWeight};
