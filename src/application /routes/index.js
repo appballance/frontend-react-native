3aec7763c6d30c5772f5cf697ca5951dc882c277
João pedro <joaof082@gmail.com>
2023-01-28 17:32:14 -0300

    feat: disabling the headers of screens
    Signed-off-by: João pedro <joaof082@gmail.com>

--- src/application /routes/index.js
+++ src/application /routes/index.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from 'react-native-paper';
 import { Register } from 'views/pages/Register';
 import { InitialSkeleton } from '../../views/pages/InitialSkeleton';
 
@@ -8,13 +9,17 @@ const Stack = createNativeStackNavigator();
 export function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="InitialSkeleton">
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="InitialSkeleton" component={InitialSkeleton} />
+      <Stack.Navigator initialRouteName="InitialSkeleton">
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen
+          name="InitialSkeleton"
+          component={InitialSkeleton}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
