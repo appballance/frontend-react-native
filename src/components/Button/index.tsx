a2c33c69cdecb71b78eae3806873478c2cdba1d9
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-22 19:08:03 -0300

    Wip
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Button() {
+  return (
+    <View>
+      <Text>Hopee</Text>
+    </View>
+  );
+}
+
+export default Button;
