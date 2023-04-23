6bb891273e46b9441830535f13a3c05ee7937635
Calenzo <calenzodevs@gmail.com>
2023-04-22 21:10:01 -0300

    feat: I was created the component Input

---
+++ src/components/Input/@types/index.ts
@@ -0,0 +1,5 @@
+export type InputProps = {
+  placeholder: string | undefined;
+  onChangeText: (text: string) => void;
+  value: string | undefined;
+};
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import * as S from './styles';
+
+import {InputProps} from './@types';
+
+const Input = ({value, placeholder, onChangeText}: InputProps) => {
+  return (
+    <S.InputContainer>
+      {!!value?.length && <S.Label>{placeholder}</S.Label>}
+      <S.Input
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+        value={value}
+      />
+    </S.InputContainer>
+  );
+};
+
+export default Input;
