2fc6c3772bfdfe7ced6ba796cdffaf6ed348fa37
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-24 02:27:52 -0300

    refactor: theme
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/commons/styles/theme.ts
+++ src/commons/styles/theme.ts
@@ -1,21 +1,18 @@
-const theme = {
-  shadow: ['0px 4px 4px rgba(0, 0, 0, 0.25)'],
+import { ITheme } from './@types';
+
+const theme: ITheme = {
   palette: {
     colors: {
-      primary: '#9448BC',
-      secondary: '#6D2494',
-      text: '#0C011B',
-      dark: {
-        gray: '#2C2834',
-      },
-      light: {
-        gray: '#47454F',
-        grayish: '#F9F7FB',
-      },
-      white: '#fff',
-      emergency: '#bc1610',
+      primary: { main: '#9448BC' },
+      secondary: { main: '#6D2494' },
+      success: { main: '#417505' },
+      error: { main: '#bc1610' },
+      text: { main: '#0C011B' },
+
+      black: { main: '#000000' },
+      white: { main: '#fff' },
+      gray: { main: '#2C2834', light: '#F9F7FB', dark: '#47454F' },
     },
   },
 };
-
 export { theme };
