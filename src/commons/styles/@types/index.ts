08934efa8ac3572732b9407090b5eab98d8eab61
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-24 02:28:20 -0300

    feat: typing types of theme
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

---
+++ src/commons/styles/@types/index.ts
@@ -0,0 +1,17 @@
+export interface IPalette {
+  colors: {
+    primary: { main: string; dark?: string; light?: string };
+    secondary: { main: string; dark?: string; light?: string };
+    success: { main: string; dark?: string; light?: string };
+    error: { main: string; dark?: string; light?: string };
+    text: { main: string; dark?: string; light?: string };
+
+    black: { main: string; dark?: string; light?: string };
+    white: { main: string; dark?: string; light?: string };
+    gray: { main: string; dark?: string; light?: string };
+  };
+}
+
+export interface ITheme {
+  palette: IPalette;
+}
