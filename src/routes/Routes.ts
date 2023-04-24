276660a7d0f3d0cd01e466784b55998517a5d07a
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-24 00:17:47 -0300

    feat: typing routes and adding stacks
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

---
+++ src/routes/Routes.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+import Register from '../pages/Register';
+// import {  } from 'react-native-screens';
+
+type Route = {
+  name: string;
+  component: React.ComponentType;
+  options?: {};
+};
+
+const routes: Route[] = [
+  {
+    name: 'Register',
+    component: Register,
+    options: { headerShown: false },
+  },
+];
+
+export { routes };
