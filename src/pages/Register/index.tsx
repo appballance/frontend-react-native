a701aa47da7177da9ea24156b73cfa935443fa42
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-24 02:31:01 -0300

    feat: starting Building page of register
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
+import Button from '../../components/elements/Button';
+import Typography from '../../components/elements/Typography';
+import { theme } from '../../commons/styles/theme';
 
 const Register = () => {
   return (
-    <View>
-      <Text>index </Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Typography
+        color={theme.palette.colors.black.main}
+        margin="10"
+        fsize="100">
+        Cadastro
+      </Typography>
+      <Button size="medium">
+        <Text>index </Text>
+      </Button>
+    </SafeAreaView>
   );
 };
 
