394cab115404a5b6ff077b5a7978a850a042e9c5
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-24 00:16:45 -0300

    feat: adding android bunlde in mainActivy
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- android/app/src/main/java/com/balance/MainActivity.java
+++ android/app/src/main/java/com/balance/MainActivity.java
@@ -1,5 +1,7 @@
 package com.balance;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -15,6 +17,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "balance";
   }
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
