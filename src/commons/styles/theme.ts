edd0a9773e8b447aff791477bad0abe0a56593c6
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-24 00:16:55 -0300

    feat: Building theme
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

---
+++ src/commons/styles/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+  shadow: ['0px 4px 4px rgba(0, 0, 0, 0.25)'],
+  palette: {
+    colors: {
+      primary: '#9448BC',
+      secondary: '#6D2494',
+      text: '#0C011B',
+      dark: {
+        gray: '#2C2834',
+      },
+      light: {
+        gray: '#47454F',
+        grayish: '#F9F7FB',
+      },
+      white: '#fff',
+      emergency: '#bc1610',
+    },
+  },
+};
+
+export { theme };
