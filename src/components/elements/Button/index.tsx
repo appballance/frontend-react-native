ab9819cc8c446e1b701509d36fe945dd618b6aac
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-25 18:10:32 -0300

    feat: adding more atributes
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 
 import * as S from './styles';
 import { IButton } from './@types';
-import Typography from '../Typography';
+import { Text } from 'react-native';
 
-function Button({ children, size }: IButton) {
+function Button({
+  children,
+  variant = 'contained',
+  width = '150px',
+  height = '100px',
+  disabled,
+  ...props
+}: IButton) {
   return (
     <S.Button
+      variant={variant}
       accessibilityRole={'button' as 'button'}
       activeOpacity={0.33}
-      $size={size}>
-      <Typography fsize={size} margin="0">
-        {children}
-      </Typography>
+      width={width}
+      height={height}
+      disabled={disabled}
+      $disabled={disabled}
+      {...props}>
+      <Text>{children}</Text>
     </S.Button>
   );
 }
