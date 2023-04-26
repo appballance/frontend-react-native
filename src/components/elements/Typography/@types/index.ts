c1c0d0c818dd058e574bb54dccc0e962a6b826c4
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-25 21:40:06 -0300

    feat: adding typing in typography
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/components/elements/Typography/@types/index.ts
+++ src/components/elements/Typography/@types/index.ts
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 export interface ITypography extends PropsWithChildren {
-  margin: string;
-  fontSize: string;
-  color: string;
+  margin?: string;
+  fontSize?: string;
+  color?: string;
 }
