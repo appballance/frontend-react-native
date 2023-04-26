23e603bca56194b1029826fdfb4a400049d41d7a
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-26 01:36:43 -0300

    feat: adding more settings in prettier
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,34 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: true,
+  jsxBracketSameLine: false,
+  insertPragma: false,
   singleQuote: true,
   trailingComma: 'all',
   endOfLine: 'auto',
+
+  overrides: [
+    {
+      files: '*.ts',
+      options: {
+        parser: 'babel',
+        functionParentheses: 'avoid',
+      },
+    },
+    {
+      files: '*.ts',
+      options: {
+        parser: 'typescript',
+        functionParentheses: 'avoid',
+      },
+    },
+    {
+      files: '*.tsx',
+      options: {
+        parser: 'typescript',
+        functionParentheses: 'avoid',
+        jsxSingleQuote: false,
+      },
+    },
+  ],
 };
