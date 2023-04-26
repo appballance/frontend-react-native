ad8830bd3d4c993bd038f16ad76f792c723f0b1d
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-25 21:38:41 -0300

    feat: adding font family in typography
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/components/elements/Typography/styles.ts
+++ src/components/elements/Typography/styles.ts
@@ -1,20 +1,15 @@
-import { css } from 'styled-components';
 import styled from 'styled-components/native';
 
 export const StyledTypgraphy = styled.Text<{
-  $margin: string;
-  $fsize: string;
+  $margin?: string;
+  $fsize?: string;
+  $fFamily?: string;
   $color?: string;
 }>`
-  font-family: 'Jost Bold';
+  font-size: ${({ $fsize }) => ($fsize ? $fsize : '16px')};
+
+  font-family: ${({ $fFamily }) => ($fFamily ? $fFamily : 'Jost-Regular')};
 
   color: ${({ $color, theme }) =>
     $color ? $color : theme.palette.colors.black.main};
-
-  ${({ $fsize }) =>
-    $fsize &&
-    css`
-      width: 150px;
-      height: 20%;
-    `}
 `;
