d83bc1ccbd70742c2d83fc4de6b52bc55afee0be
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-26 01:38:56 -0300

    feat: adding formik and tipyng
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,14 +1,37 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import React from 'react';
 import Typography from '../../components/elements/Typography';
 import Button from '../../components/elements/Button';
 
+import * as S from './styles';
+import Input from '../../components/Input';
+import { Formik } from 'formik';
+import { IRegisterForm } from './@types/formikTypes';
+import FormRegister from './Forms';
+
 const Register = () => {
+  const initialValues = {
+    surname: '',
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  } as IRegisterForm;
+
+  const handleSubmit = async (values: IRegisterForm) => { };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Typography color="red" fontSize="50px" margin="0">
-        Cadastro
-      </Typography>
+      <S.ContainerHeader>
+        <Typography fontSize="25px" margin="0">
+          Cadastro
+        </Typography>
+      </S.ContainerHeader>
+      <S.ContainerForms>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          {({ }) => <FormRegister />}
+        </Formik>
+      </S.ContainerForms>
       <Button width="100px" height="30px">
         cadastrar
       </Button>
