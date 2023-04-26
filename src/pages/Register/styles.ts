3f12192f198fd0185ddf2044c4bbc46ab9451399
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-04-26 01:39:17 -0300

    feat: Building containers
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -3,3 +3,13 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const ContainerHeader = styled.View`
+  width: 100%;
+
+  align-items: center;
+`;
+
+export const ContainerForms = styled.View`
+  margin: 0px 54px;
+`;
