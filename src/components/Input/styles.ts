7a03fa49aa67cb365b4ab0ef5f4a36b5f0c03574
João Pedro <joaof082@gmail.com>
2023-05-11 21:06:45 -0300

    feat: adding condition of margin and color
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components/native';
 
-export const InputContainer = styled.View`
+export const InputContainer = styled.View<{ marginBottom?: string }>`
   display: flex;
   flex-direction: column;
   border-bottom-width: 1px;
   border-bottom-color: #141b41;
+
+  margin-bottom: ${({ marginBottom }) => (marginBottom ? marginBottom : 0)};
 `;
 
-export const Label = styled.Text``;
+export const Label = styled.Text<{ labelColor?: string }>`
+  color: ${({ labelColor }) => (labelColor ? labelColor : 'white')};
+`;
 
 export const Input = styled.TextInput`
   width: 100%;
   height: 50px;
   padding-left: 10px;
+  color: black;
 `;
