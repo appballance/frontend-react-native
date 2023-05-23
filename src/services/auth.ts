73fb7d3e2a85c1ca5a2ac645656278e23461a6cd
João Pedro <joaof082@gmail.com>
2023-05-23 20:56:57 -0300

    feat: Building services of auth
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/services/auth.ts
@@ -0,0 +1,30 @@
+import { IAuthenticateUser, IServicePost } from './@types';
+import { ServiceGet, ServicePost } from './api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const authenticateUser = async ({
+  email,
+  password,
+}: IAuthenticateUser) => {
+  const isAuth = false;
+  const response: IServicePost = await ServicePost(
+    'auth',
+    {
+      email,
+      password,
+    },
+    isAuth,
+  );
+
+  if (!response?.token) {
+    return response;
+  }
+
+  AsyncStorage.setItem('token', response.token);
+  return response;
+};
+
+export const isAuthenticated = async () => {
+  const response = await ServiceGet('user');
+  return response?.detail ? false : true;
+};
