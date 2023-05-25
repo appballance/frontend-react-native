74c7f9b5bfcbab5e12a5da7356437129190d15e5
João Pedro <joaof082@gmail.com>
2023-05-25 20:22:18 -0300

    feat: adding right icon in form password
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,26 +3,43 @@ import React from 'react';
 import * as S from './styles';
 
 import { InputProps } from './@types';
+import { Text, View } from 'react-native';
+
+import Button from '../elements/Button';
 
 const Input = ({
   value,
+  height,
   placeholder,
   onChangeText,
   marginBottom,
   labelColor,
+  IconRight,
   ...props
 }: InputProps) => {
   return (
-    <S.InputContainer marginBottom={marginBottom}>
-      {!!value?.length && (
-        <S.Label labelColor={labelColor}>{placeholder}</S.Label>
+    <S.InputContainer marginBottom={marginBottom} height={height}>
+      <View style={{ flexDirection: 'column', width: '90%' }}>
+        {!!value?.length && (
+          <S.Label labelColor={labelColor}>{placeholder}</S.Label>
+        )}
+        <S.Input
+          placeholder={placeholder}
+          onChangeText={onChangeText}
+          value={value}
+          {...props}
+        />
+      </View>
+      {IconRight && (
+        <View
+          style={{
+            justifyContent: 'center',
+          }}>
+          <Button height="20px" width="20px" variant="text">
+            <Text>O</Text>
+          </Button>
+        </View>
       )}
-      <S.Input
-        placeholder={placeholder}
-        onChangeText={onChangeText}
-        value={value}
-        {...props}
-      />
     </S.InputContainer>
   );
 };
