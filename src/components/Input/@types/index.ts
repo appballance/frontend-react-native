ad139001ca999d5392e7a4209bd1013ac1b08b19
João Pedro <joaof082@gmail.com>
2023-05-25 20:23:20 -0300

    feat: typing Input props
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/components/Input/@types/index.ts
+++ src/components/Input/@types/index.ts
@@ -1,9 +1,12 @@
+import React from 'react';
 import { TextInputProps } from 'react-native';
 
 export interface InputProps extends TextInputProps {
   placeholder: string | undefined;
   onChangeText: (text: string) => void;
   value: string | undefined;
+  height?: string;
   marginBottom?: string;
   labelColor?: string;
+  IconRight?: any;
 }
