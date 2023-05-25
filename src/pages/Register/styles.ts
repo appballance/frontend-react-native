82fb9b54226fa2395d676f4c65e73854005a1fbc
João Pedro <joaof082@gmail.com>
2023-05-25 20:27:37 -0300

    feat: Changing style of register
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,43 +1,49 @@
 import styled from 'styled-components/native';
 import Button from '../../components/elements/Button';
+import { theme } from '../../commons/styles/theme';
 
 export const Container = styled.View`
   flex: 1;
-
-  justify-content: center;
-  margin: 0;
-  padding: 0;
+  background-color: #ffff;
   width: 100%;
-  height: 100%;
+  justify-content: center;
+`;
+
+export const StScrollView = styled.ScrollView`
+  height: 500px;
+  background-color: #ffff;
 `;
 
 export const ContainerHeader = styled.View`
   width: 100%;
-  height: 80px;
-  justify-content: center;
-  background-color: aqua;
+  height: 50px;
+  flex: 1;
+  justify-content: flex-end;
+  /* background-color: aqua; */
   align-items: center;
 `;
 
 export const ContainerForms = styled.View`
-  margin: 0px 45px;
-  height: 55%;
+  margin: 0px 25px;
+  height: 400px;
   justify-content: center;
-  margin-bottom: 5%;
+  /* margin-bottom: 5%; */
+  /* background-color: red; */
 `;
 
 export const ContainerButton = styled.View`
   width: 100%;
-  height: 40px;
+  height: 100px;
+  flex: 1;
   align-items: center;
-  background-color: aqua;
   justify-content: center;
 `;
 
-export const StyledButtonLoading = styled(Button)`
+export const StyledButtonLoading = styled(Button).attrs({
+  color: theme.palette.colors.white.main,
+})`
   width: 200px;
   height: 40px;
-
-  color: ${({ theme }) => theme.palette.colors.white.main};
   font-size: 18px;
+  font-family: ${({ theme }) => theme.typography.regular};
 `;
