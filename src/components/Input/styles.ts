6aa442d452d96dbb770449290215b5c697d7229a
João Pedro <joaof082@gmail.com>
2023-05-25 20:24:00 -0300

    feat: Changing styles of input
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components/native';
 
-export const InputContainer = styled.View<{ marginBottom?: string }>`
-  display: flex;
-  flex-direction: column;
+export const InputContainer = styled.View<{
+  marginBottom?: string;
+  height?: string;
+}>`
+  height: ${({ height }) => (height ? height : 'auto')};
+  min-height: 50px;
+  flex-direction: row;
   border-bottom-width: 1px;
   border-bottom-color: #141b41;
 
@@ -14,8 +18,7 @@ export const Label = styled.Text<{ labelColor?: string }>`
 `;
 
 export const Input = styled.TextInput`
-  width: 100%;
-  height: 50px;
+  width: 80%;
   padding-left: 10px;
   color: black;
 `;
